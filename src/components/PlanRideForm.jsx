451bc110efa8b7dfcf2abfa4ab057d25427448be
JosephSamuel2022 <josephsamuelm2021@gmail.com>
2023-10-22 20:39:20 +0530

    react select

--- src/components/PlanRideForm.jsx
+++ src/components/PlanRideForm.jsx
@@ -4,11 +4,15 @@ import { useNavigate } from "react-router-dom";
 import { database } from "../firebase-config";
 import { getDatabase, ref, get } from "firebase/database";
 import { useEffect } from "react";
+import Select from "react-select";
 
 const PlanRideForm = () => {
 	const navigate = useNavigate();
-	const [locationNameFrom, setFromLocation] = useState("chrompet");
-	const [locationNameTo, setToLocation] = useState("t nagar");
+	const [locationNameFrom, setFromLocation] = useState(null);
+	const [locationNameTo, setToLocation] = useState(null);
+
+	const [lNameFrom, setFromLocations] = useState(null);
+	const [lNameTo, setToLocations] = useState(null);
 
 	const [locationDataFrom, setLocationDataFrom] = useState(null);
 	const [locationDataTo, setLocationDataTo] = useState(null);
@@ -71,6 +75,73 @@ const PlanRideForm = () => {
 		console.log(locationDataTo);
 	};
 
+	const options = [
+		{
+			value: "chennai international airport (maa)",
+			label: "Chennai International Airport (MAA)",
+		},
+		{
+			value: "chennai central railway station",
+			label: "Chennai Central Railway Station",
+		},
+		{ value: "egmore railway station", label: "Egmore Railway Station" },
+		{ value: "marina beach", label: "Marina Beach" },
+		{ value: "besant nagar beach", label: "Besant Nagar Beach" },
+		{ value: "kapaleeshwarar temple", label: "Kapaleeshwarar Temple" },
+		{ value: "vadapalani murugan temple", label: "Vadapalani Murugan Temple" },
+		{ value: "ashtalakshmi temple", label: "Ashtalakshmi Temple" },
+		{
+			value: "parthasarathy temple, triplicane",
+			label: "Parthasarathy Temple, Triplicane",
+		},
+		{ value: "government museum, egmore", label: "Government Museum, Egmore" },
+		{ value: "valluvar kottam", label: "Valluvar Kottam" },
+		{ value: "guindy national park", label: "Guindy National Park" },
+		{ value: "semmozhi poonga", label: "Semmozhi Poonga" },
+		{ value: "santhome basilica", label: "Santhome Basilica" },
+		{ value: "koyambedu bus terminus", label: "Koyambedu Bus Terminus" },
+		{ value: "express avenue mall", label: "Express Avenue Mall" },
+		{ value: "phoenix marketcity", label: "Phoenix MarketCity" },
+		{ value: "spencer plaza", label: "Spencer Plaza" },
+		{ value: "t nagar", label: "T Nagar (Shopping District)" },
+		{ value: "mylapore", label: "Mylapore" },
+		{ value: "nanganallur", label: "Nanganallur" },
+		{ value: "thiruvanmiyur", label: "Thiruvanmiyur" },
+		{ value: "adyar", label: "Adyar" },
+		{ value: "royapettah", label: "Royapettah" },
+		{
+			value: "t nagar ranganathan street",
+			label: "T Nagar Ranganathan Street",
+		},
+		{ value: "pallikaranai marsh", label: "Pallikaranai Marsh" },
+		{ value: "anna university", label: "Anna University" },
+		{ value: "iit madras", label: "IIT Madras" },
+		{ value: "madras high court", label: "Madras High Court" },
+		{
+			value: "theosophical society adyar",
+			label: "Theosophical Society Adyar",
+		},
+		{ value: "royapuram fishing harbour", label: "Royapuram Fishing Harbour" },
+		{ value: "st thomas mount", label: "St Thomas Mount" },
+		{ value: "birla planetarium", label: "Birla Planetarium" },
+		{ value: "chetpet ecopark", label: "Chetpet Ecopark" },
+		{ value: "ripon building", label: "Ripon Building" },
+		{ value: "chennai snake park", label: "Chennai Snake Park" },
+		{ value: "brahmapuram reservoir", label: "Brahmapuram Reservoir" },
+		{ value: "kishkinta theme park", label: "Kishkinta Theme Park" },
+		{ value: "kovalam beach", label: "Kovalam Beach" },
+		{
+			value: "mahabalipuram (mamallapuram)",
+			label: "Mahabalipuram (Mamallapuram)",
+		},
+	];
+	const filteredOptionsFrom = options.filter(
+		(option) => option.value !== locationNameTo
+	);
+	const filteredOptionsTo = options.filter(
+		(option) => option.value !== locationNameFrom
+	);
+
 	return (
 		<div>
 			<h1 className='heading'>Plan your ride</h1>
@@ -78,24 +149,32 @@ const PlanRideForm = () => {
 				<div className='ride-details'>
 					<div className='from-input'>
 						<div className='circle'></div>
-						<input
-							type='text'
-							placeholder='From Location'
+						<Select
+							className='custom-select'
+							options={filteredOptionsFrom}
+							placeholder='Select From Location'
 							spellCheck='false'
-							value={locationNameFrom}
-							onChange={(e) => setFromLocation(e.target.value)}
+							defaultValue={lNameFrom}
+							onChange={(selectedOption) => {
+								setFromLocations(selectedOption); // Update lNameTo
+								setFromLocation(selectedOption.value); // Update locationNameTo
+							}}
 						/>
 					</div>
 				</div>
 				<div className='ride-details'>
 					<div className='to-input'>
 						<div className='square'></div>
-						<input
-							type='text'
-							placeholder='To Location'
+						<Select
+							className='custom-select'
+							options={filteredOptionsTo}
+							placeholder='Select To Location'
 							spellCheck='false'
-							value={locationNameTo}
-							onChange={(e) => setToLocation(e.target.value)}
+							defaultValue={lNameTo}
+							onChange={(selectedOption) => {
+								setToLocations(selectedOption); // Update lNameTo
+								setToLocation(selectedOption.value); // Update locationNameTo
+							}}
 						/>
 					</div>
 				</div>
